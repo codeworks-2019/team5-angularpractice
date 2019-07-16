f02c49861e773e681fa795b0790bf3751bd7748a
pilsburryDoughBoi <52458361+pilsburryDoughBoi@users.noreply.github.com>
2019-07-16 10:54:03 -0400

    Update java-script.js

--- java-script.js
+++ java-script.js
@@ -1,5 +1,10 @@
-var
-var
+var inputElement = document.getElementById('dataToBind');
+var showElement = document.getElementById('showBoundData');
+
+// add an event listner to listen to keyuip event
+inputElement.addEventlistener('keyup', function() {
+  showElement.innerHTML = inputElement.value.toString();
+});
 
 
 
